e2c3bdca4771b5243de335594bdf7676deb0959c
Kevin Chang <kevinjchang98@gmail.com>
2022-11-08 13:30:16 -0800

    refactor common html elements to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,25 @@
+import Head from 'next/head';
 import '../styles/globals.css';
+import styles from '../styles/Home.module.css';
 
 export default function RootLayout({ children }: any) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <Head>
+                <title>St. Francis House</title>
+                <meta name="description" content="St. Francis House" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <body>
+                <div className={styles.container}>
+                    <main className={styles.main}>{children}</main>
+                </div>
+            </body>
+
+            <footer className={styles.footer}>
+                <p>Footer content</p>
+            </footer>
         </html>
     );
 }
