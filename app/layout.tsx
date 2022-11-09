5d9a25b7f388a6767d5a32d1a29d2aaa97e3f7cd
Kevin Chang <kevinjchang98@gmail.com>
2022-11-09 00:44:24 -0800

    add settings context

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,20 @@
 import Head from 'next/head';
 import { useState } from 'react';
 import NavBar from '../components/NavBar/NavBar';
+import { Settings, SettingsContext } from '../contexts/SettingsContext';
 import { SignInContext } from '../contexts/SignInContext';
 import '../styles/globals.css';
 import styles from '../styles/Home.module.css';
 
 export default function RootLayout({ children }: any) {
     const [isSignedIn, setIsSignedIn] = useState<boolean>(false);
+    const [settings, setSettings] = useState<Settings>({
+        daysEarlyThreshold: 25,
+        backpackThreshold: 91,
+        sleepingBagThreshold: 182,
+        earlyOverride: false,
+    });
+
     return (
         <html lang="en">
             <Head>
@@ -19,12 +27,18 @@ export default function RootLayout({ children }: any) {
 
             <body>
                 <SignInContext.Provider value={{ isSignedIn, setIsSignedIn }}>
-                    <div className={styles.container}>
-                        <NavBar />
-                        <main className={styles.main}>
-                            {isSignedIn ? children : <h1>Please sign in</h1>}
-                        </main>
-                    </div>
+                    <SettingsContext.Provider value={{ settings, setSettings }}>
+                        <div className={styles.container}>
+                            <NavBar />
+                            <main className={styles.main}>
+                                {isSignedIn ? (
+                                    children
+                                ) : (
+                                    <h1>Please sign in</h1>
+                                )}
+                            </main>
+                        </div>
+                    </SettingsContext.Provider>
                 </SignInContext.Provider>
             </body>
 
