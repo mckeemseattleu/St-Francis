3cb23cf95e164e5fcb8d79dece99114ea4952daf
Kevin Chang <kevinjchang98@gmail.com>
2022-11-09 01:19:06 -0800

    fix incorrect mutation of settings state in form

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -9,6 +9,55 @@ export default function Settings() {
     return (
         <>
             <h1>Settings</h1>
+
+            {setSettings ? (
+                <>
+                    <h2>Days early threshold</h2>
+                    <input
+                        type="number"
+                        name="daysEarlyThreshold"
+                        id="daysEarlyThreshold"
+                        value={settings.daysEarlyThreshold}
+                        onChange={(e) => {
+                            setSettings((prev: typeof settings) => ({
+                                ...prev,
+                                daysEarlyThreshold: e.target.value,
+                            }));
+                        }}
+                    />
+                    <br />
+
+                    <h2>Backpack threshold</h2>
+                    <input
+                        type="number"
+                        name="backpackThreshold"
+                        id="backpackThreshold"
+                        value={settings.backpackThreshold}
+                        onChange={(e) => {
+                            setSettings((prev: typeof settings) => ({
+                                ...prev,
+                                backpackThreshold: e.target.value,
+                            }));
+                        }}
+                    />
+                    <br />
+
+                    <h2>Sleeping bag threshold</h2>
+                    <input
+                        type="number"
+                        name="sleepingBagThreshold"
+                        id="sleepingBagThreshold"
+                        value={settings.sleepingBagThreshold}
+                        onChange={(e) => {
+                            setSettings((prev: typeof settings) => ({
+                                ...prev,
+                                sleepingBagThreshold: e.target.value,
+                            }));
+                        }}
+                    />
+                    <br />
+                </>
+            ) : null}
         </>
     );
 }
