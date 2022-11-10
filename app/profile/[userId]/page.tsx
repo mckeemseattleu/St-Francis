1e7a85f6a810ec8650c205ae3c86e54679599807
Kevin Chang <kevinjchang98@gmail.com>
2022-11-09 23:44:07 -0800

    create profile page

---
+++ app/profile/[userId]/page.tsx
@@ -0,0 +1,21 @@
+export interface ClientDoc {
+    firstName: string;
+    lastName: string;
+    middleInitial: string;
+    birthday: string; // TODO: Consider saving as timestamp
+    gender: string;
+    race: string;
+    postalCode: string;
+    numKids: number;
+    notes: string;
+    isCheckedIn: boolean;
+    isBanned: boolean;
+}
+
+export default function Profile() {
+    return (
+        <>
+            <h1>Profile</h1>
+        </>
+    );
+}
