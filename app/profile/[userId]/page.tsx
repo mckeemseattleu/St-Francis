493e6688b43f281ed0fe7adb7a1c650b142c23ea
Kevin Chang <kevinjchang98@gmail.com>
2022-11-09 23:50:07 -0800

    get and display info in profile page

--- app/profile/[userId]/page.tsx
+++ app/profile/[userId]/page.tsx
@@ -1,3 +1,10 @@
+'use client';
+
+import { doc, getDoc } from 'firebase/firestore';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { firestore } from '../../../firebase/firebase';
+
 export interface ClientDoc {
     firstName: string;
     lastName: string;
@@ -12,10 +19,56 @@ export interface ClientDoc {
     isBanned: boolean;
 }
 
-export default function Profile() {
+interface ProfileProps {
+    params: { userId: string };
+}
+
+export default function Profile({ params }: ProfileProps) {
+    const router = useRouter();
+    const [clientData, setClientData] = useState<ClientDoc>();
+
+    useEffect(() => {
+        getClientData();
+    }, []);
+
+    const getClientData = async () => {
+        const clientDoc = await getDoc(
+            doc(firestore, 'clients', params.userId)
+        );
+
+        if (clientDoc.exists()) {
+            setClientData({
+                firstName: clientDoc.data().firstName,
+                lastName: clientDoc.data().lastName,
+                middleInitial: clientDoc.data().middleInitial,
+                birthday: clientDoc.data().birthday,
+                gender: clientDoc.data().gender,
+                race: clientDoc.data().race,
+                postalCode: clientDoc.data().postalCode,
+                numKids: clientDoc.data().numKids,
+                notes: clientDoc.data().notes,
+                isCheckedIn: clientDoc.data().isCheckedIn,
+                isBanned: clientDoc.data().isBanned,
+            });
+        } else {
+            router.push('/');
+        }
+    };
+
     return (
         <>
             <h1>Profile</h1>
+
+            <h2>{`${clientData?.firstName} ${clientData?.middleInitial} ${clientData?.lastName}`}</h2>
+            <p>Birthday: {clientData?.birthday}</p>
+            <p>Gender: {clientData?.gender}</p>
+            <p>Race: {clientData?.race}</p>
+            <p>Postal code: {clientData?.postalCode}</p>
+            <p>Number of kids: {clientData?.numKids}</p>
+            <p> Notes:</p>
+            <p>{clientData?.notes}</p>
+            <p>{clientData?.isCheckedIn ? 'Checked in' : 'Not checked in'}</p>
+            <p>{clientData?.isBanned ? 'Banned' : 'Not banned'}</p>
         </>
     );
 }
