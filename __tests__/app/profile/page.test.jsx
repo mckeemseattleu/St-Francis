d432c5028d4b7e09da9a7af5dde816fa94e58f92
Kevin Chang <kevinjchang98@gmail.com>
2022-11-13 18:19:23 -0800

    add basic firestore test

--- __tests__/app/profile/page.test.jsx
+++ __tests__/app/profile/page.test.jsx
@@ -1,10 +1,47 @@
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Profile from '../../../app/profile/[userId]/page';
 
 jest.mock('next/navigation', () => ({
     __esModule: true,
     useRouter: jest.fn(),
+}));
+
+// Mock the required firestore functionality in the component
+jest.mock('firebase/firestore', () => ({
+    __esModule: true,
+    // Needed because of firebase.ts import
+    getFirestore: jest.fn(),
+    doc: jest.fn(),
+    getDoc: jest.fn(() => ({
+        // Needed for clientDoc.exists() check
+        exists: () => {
+            return true;
+        },
+        // Needed for clientDoc.data().whatever calls
+        data: () => ({
+            firstName: 'testFirst',
+            lastName: 'testLast',
+            middleInitial: 't',
+            birthday: '',
+            gender: '',
+            race: '',
+            postalCode: '',
+            numKids: 2,
+            notes: '',
+            isCheckedIn: false,
+            isBanned: false,
+        }),
+    })),
+}));
+
+jest.mock('firebase/compat/app', () => ({
+    __esModule: true,
+    default: {
+        apps: [],
+        initializeApp: () => {},
+        auth: () => {},
+    },
 }));
 
 describe('Profile page', () => {
@@ -16,5 +53,18 @@ describe('Profile page', () => {
         });
 
         expect(title).toBeInTheDocument();
+    });
+
+    it('renders name correctly', async () => {
+        // TODO: Gives warning because of async useEffect()
+        await act(async () => {
+            render(<Profile params={{ userId: '1234' }} />);
+        });
+
+        const firstName = screen.getByRole('heading', {
+            name: 'testFirst t testLast',
+        });
+
+        expect(firstName).toBeInTheDocument();
     });
 });
