1ccfbb17b1b600bffcdb720c085254cb80db73ff
Kevin Chang <kevinjchang98@gmail.com>
2022-12-14 12:26:30 -0800

    refactor client data fields to one object

--- components/ClientInfoForm/ClientInfoForm.tsx
+++ components/ClientInfoForm/ClientInfoForm.tsx
@@ -1,23 +1,12 @@
 import { addDoc, collection, doc, setDoc } from 'firebase/firestore';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { ClientDoc } from '../../app/profile/[userId]/page';
 import { firestore } from '../../firebase/firebase';
 
 interface ClientInfoFormProps {
     id?: string;
-    initialData?: {
-        firstName: string;
-        lastName: string;
-        middleInitial: string;
-        birthday: string; // TODO: Consider saving as timestamp
-        gender: string;
-        race: string;
-        postalCode: string;
-        numKids: number;
-        notes: string;
-        isCheckedIn: boolean;
-        isBanned: boolean;
-    };
+    initialData?: ClientDoc;
     redirect?: string;
 }
 
@@ -36,61 +25,32 @@ export default function ClientInfoForm({
     initialData = undefined,
     redirect = '/',
 }: ClientInfoFormProps) {
-    // Use old info from props if it exists, else default values
-    const [firstName, setFirstName] = useState<string>(
-        initialData ? initialData.firstName : ''
-    );
-    const [lastName, setLastName] = useState<string>(
-        initialData ? initialData.lastName : ''
-    );
-    const [middleInitial, setMiddleInitial] = useState<string>(
-        initialData ? initialData.middleInitial : ''
-    );
-    const [birthday, setBirthday] = useState<any>(
+    // Use default values if no initial data was passed in
+    const [clientData, setClientData] = useState<ClientDoc>(
         initialData
-            ? initialData.birthday
-            : new Date().toISOString().substring(0, 10)
+            ? initialData
+            : {
+                  firstName: '',
+                  lastName: '',
+                  middleInitial: '',
+                  birthday: new Date().toISOString().substring(0, 10), // TODO: Consider saving as timestamp
+                  gender: '',
+                  race: '',
+                  postalCode: '',
+                  numKids: 0,
+                  notes: '',
+                  isCheckedIn: false,
+                  isBanned: false,
+              }
     );
-    const [gender, setGender] = useState<string>(
-        initialData ? initialData.gender : ''
-    );
-    const [race, setRace] = useState<string>(
-        initialData ? initialData.race : ''
-    );
-    const [postalCode, setPostalCode] = useState<string>(
-        initialData ? initialData.postalCode : ''
-    );
-    const [numKids, setNumKids] = useState<number>(
-        initialData ? initialData.numKids : 0
-    );
-    const [notes, setNotes] = useState<string>(
-        initialData ? initialData.notes : ''
-    );
-    const [isCheckedIn, setIsCheckedIn] = useState<boolean>(
-        initialData ? initialData.isCheckedIn : false
-    );
-    const [isBanned, setIsBanned] = useState<boolean>(
-        initialData ? initialData.isBanned : false
-    );
+
     const router = useRouter();
 
     // Creates a new doc with an automatically generated id for the client
     const addNewClient = async () => {
         // Only create if either first or last name is not empty
-        if (firstName !== '' || lastName !== '') {
-            await addDoc(collection(firestore, 'clients'), {
-                firstName,
-                lastName,
-                middleInitial,
-                birthday, // TODO: Consider saving as timestamp
-                gender,
-                race,
-                postalCode,
-                numKids,
-                notes,
-                isCheckedIn: false,
-                isBanned,
-            });
+        if (clientData.firstName !== '' || clientData.lastName !== '') {
+            await addDoc(collection(firestore, 'clients'), clientData);
         }
 
         // Redirect back to specified redirect route
@@ -102,19 +62,7 @@ export default function ClientInfoForm({
     const updateClientData = async () => {
         // Ensure id's not undefined
         if (id) {
-            await setDoc(doc(firestore, 'clients', id), {
-                firstName,
-                lastName,
-                middleInitial,
-                birthday, // TODO: Consider saving as timestamp
-                gender,
-                race,
-                postalCode,
-                numKids,
-                notes,
-                isCheckedIn,
-                isBanned,
-            });
+            await setDoc(doc(firestore, 'clients', id), clientData);
 
             // Redirect to specified route
             router.push(redirect);
@@ -135,19 +83,22 @@ export default function ClientInfoForm({
                 type="checkbox"
                 name="isBanned"
                 id="isBanned"
-                defaultChecked={isBanned}
-                value={isBanned ? 'on' : 'off'}
+                defaultChecked={clientData.isBanned}
+                value={clientData.isBanned ? 'on' : 'off'}
                 onChange={(e) => {
-                    setIsBanned(e.target.checked);
+                    setClientData({
+                        ...clientData,
+                        isBanned: e.target.checked,
+                    });
                 }}
             />
 
             <h2>First name</h2>
             <input
                 type="text"
-                value={firstName}
+                value={clientData.firstName}
                 onChange={(e) => {
-                    setFirstName(e.target.value);
+                    setClientData({ ...clientData, firstName: e.target.value });
                 }}
             />
             <br />
@@ -155,9 +106,12 @@ export default function ClientInfoForm({
             <h2>Middle initial</h2>
             <input
                 type="text"
-                value={middleInitial}
+                value={clientData.middleInitial}
                 onChange={(e) => {
-                    setMiddleInitial(e.target.value);
+                    setClientData({
+                        ...clientData,
+                        middleInitial: e.target.value,
+                    });
                 }}
             />
             <br />
@@ -165,9 +119,9 @@ export default function ClientInfoForm({
             <h2>Last name</h2>
             <input
                 type="text"
-                value={lastName}
+                value={clientData.lastName}
                 onChange={(e) => {
-                    setLastName(e.target.value);
+                    setClientData({ ...clientData, lastName: e.target.value });
                 }}
             />
             <br />
@@ -177,18 +131,18 @@ export default function ClientInfoForm({
                 type="date"
                 name="birthday"
                 id="birthday"
-                value={birthday}
+                value={clientData.birthday}
                 onChange={(e) => {
-                    setBirthday(e.target.value);
+                    setClientData({ ...clientData, birthday: e.target.value });
                 }}
             />
 
             <h2>Gender</h2>
             <input
                 type="text"
-                value={gender}
+                value={clientData.gender}
                 onChange={(e) => {
-                    setGender(e.target.value);
+                    setClientData({ ...clientData, gender: e.target.value });
                 }}
             />
             <br />
@@ -196,9 +150,9 @@ export default function ClientInfoForm({
             <h2>Race</h2>
             <input
                 type="text"
-                value={race}
+                value={clientData.race}
                 onChange={(e) => {
-                    setRace(e.target.value);
+                    setClientData({ ...clientData, race: e.target.value });
                 }}
             />
             <br />
@@ -206,9 +160,12 @@ export default function ClientInfoForm({
             <h2>Postal code</h2>
             <input
                 type="text"
-                value={postalCode}
+                value={clientData.postalCode}
                 onChange={(e) => {
-                    setPostalCode(e.target.value);
+                    setClientData({
+                        ...clientData,
+                        postalCode: e.target.value,
+                    });
                 }}
             />
             <br />
@@ -216,18 +173,21 @@ export default function ClientInfoForm({
             <h2>Number of kids</h2>
             <input
                 type="number"
-                value={numKids}
+                value={clientData.numKids}
                 onChange={(e) => {
-                    setNumKids(parseInt(e.target.value));
+                    setClientData({
+                        ...clientData,
+                        numKids: parseInt(e.target.value),
+                    });
                 }}
             />
             <br />
 
             <h2>Notes</h2>
             <textarea
-                value={notes}
+                value={clientData.notes}
                 onChange={(e) => {
-                    setNotes(e.target.value);
+                    setClientData({ ...clientData, notes: e.target.value });
                 }}
                 cols={40}
                 rows={5}
