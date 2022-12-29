fc0f7b795c5c05528b00ebb994bd0433a3c74947
Kevin Chang <kevinjchang98@gmail.com>
2022-12-28 17:54:06 -0800

    create printout component and page

--- app/profile/[userId]/visit/[visitId]/printout/page.tsx
+++ app/profile/[userId]/visit/[visitId]/printout/page.tsx
@@ -3,6 +3,9 @@
 import { doc, getDoc } from 'firebase/firestore';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import PrintoutForm, {
+    FormItem,
+} from '../../../../../../components/PrintoutForm/PrintoutForm';
 import { firestore } from '../../../../../../firebase/firebase';
 import { VisitDoc } from '../../../../../checkin/[userId]/page';
 import { ClientDoc } from '../../../page';
@@ -15,12 +18,18 @@ export default function Printout({ params }: PrintoutProps) {
     const router = useRouter();
     const [visitData, setVisitData] = useState<VisitDoc>();
     const [clientData, setClientData] = useState<ClientDoc>();
+    const [formData, setFormData] = useState<Array<FormItem>>();
 
     // Get visit data on component load
     useEffect(() => {
         getClientData();
         getVisitData();
     }, []);
+
+    // TODO: Convert to waiting for both to finish using Promises to prevent doing this twice
+    useEffect(() => {
+        formatData();
+    }, [visitData, clientData]);
 
     // Gets the client's document from firestore based on route's clientId
     const getClientData = async () => {
@@ -48,19 +57,110 @@ export default function Printout({ params }: PrintoutProps) {
         }
     };
 
-    return (
-        <>
-            <h1>{`${clientData?.firstName} ${clientData?.lastName}'s Shopping List`}</h1>
-            <h1>
-                {
-                    // Uses timestamp, if undefined uses today's date
-                    new Date(
-                        visitData
-                            ? visitData?.timestamp?.seconds * 1000
-                            : new Date()
-                    ).toDateString()
-                }
-            </h1>
-        </>
-    );
+    // Formats the data into a form <PrintoutForm /> expects
+    const formatData = () => {
+        if (!visitData) return;
+
+        // Keys we consider "Special Requests"
+        const specialRequestItemKeys = [
+            'backpack',
+            'sleepingBag',
+            'busTicket',
+            'giftCard',
+            'financialAssistance',
+        ];
+
+        // What we want the form to label each checkbox
+        const itemLabels = {
+            clothingMen: 'Mens',
+            clothingWomen: 'Womens',
+            clothingBoy: 'Kids (Boy)',
+            clothingGirl: 'Kids (Girl)',
+            backpack: 'Backpack',
+            sleepingBag: 'Sleeping Bag',
+            busTicket: 'Bus Ticket',
+            giftCard: 'Gift Card',
+            diaper: '',
+            financialAssistance: 'Financial Assistance',
+        };
+
+        let ans: typeof formData = [];
+
+        // Get clothing items to show
+        let clothingItems: Array<string> = [];
+
+        Object.keys(visitData).forEach((key) => {
+            if (
+                key.startsWith('clothing') &&
+                visitData[key as keyof typeof visitData]
+            ) {
+                clothingItems.push(itemLabels[key as keyof typeof itemLabels]);
+            }
+        });
+
+        // Get special request items to show
+        let specialRequestItems: Array<string> = [];
+
+        Object.keys(visitData).forEach((key) => {
+            if (
+                specialRequestItemKeys.includes(key) &&
+                visitData[key as keyof typeof visitData] != 0
+            ) {
+                specialRequestItems.push(
+                    itemLabels[key as keyof typeof itemLabels]
+                );
+            }
+        });
+
+        // Add clothing items
+        if (clothingItems.length != 0) {
+            ans.push({
+                title: 'Clothing',
+                type: 'checkbox',
+                items: clothingItems,
+            });
+        }
+
+        // Add special request items
+        if (specialRequestItems.length != 0) {
+            ans.push({
+                title: 'Special Requests',
+                type: 'checkbox',
+                items: specialRequestItems,
+            });
+        }
+
+        // Add household items if present
+        if (visitData.household.length > 0) {
+            ans.push({
+                title: 'Household',
+                type: 'text',
+                items: [visitData.household],
+            });
+        }
+
+        // Add diapers if present
+        if (visitData.diaper != 0) {
+            ans.push({ title: 'Diapers', type: 'checkbox', items: [''] });
+        }
+
+        // Add notes if present
+        if (visitData.notes.length > 0) {
+            ans.push({
+                title: 'Notes',
+                type: 'text',
+                items: [visitData.notes],
+            });
+        }
+
+        setFormData(ans);
+    };
+
+    return clientData && visitData && formData ? (
+        <PrintoutForm
+            clientData={clientData}
+            visitData={visitData}
+            data={formData}
+        />
+    ) : null;
 }
---
+++ components/PrintoutForm/PrintoutForm.tsx
@@ -0,0 +1,83 @@
+import { VisitDoc } from '../../app/checkin/[userId]/page';
+import { ClientDoc } from '../../app/profile/[userId]/page';
+import styles from './PrintoutForm.module.css';
+
+interface PrintoutFormProps {
+    // TODO: We can ask for form title instead of clientData and update
+    // component doc
+    clientData: ClientDoc;
+    // TODO: We can ask for date instead of visitData and update component
+    // comment
+    visitData: VisitDoc;
+    data: Array<FormItem>;
+}
+
+export interface FormItem {
+    title: string;
+    type: 'checkbox' | 'text';
+    items: Array<string>;
+}
+
+/**
+ * Takes in data in the form of an array of FormItem objects and generates a
+ * printout based of that. A FormItem object might look like the below for a
+ * section with a title of 'Clothing' and two checkboxes 'Mens' and 'Womens':
+ * {
+ *   title: 'Clothing',
+ *   type: 'checkbox',
+ *   items: ['Mens', 'Womens']
+ * }
+ *
+ * @param clientData Only used for form title
+ * @param visitData Only used for date in title section
+ * @param data An array of FormItem objects to generate the printout from
+ */
+export default function PrintoutForm({
+    clientData,
+    visitData,
+    data,
+}: PrintoutFormProps) {
+    const bodyItems = data.map((section: any) => {
+        const content =
+            // TODO: Refactor from tertiary to switch case in case we have more
+            // than 2 possible values for section.type in the future
+            section.type === 'checkbox'
+                ? section.items.map((item: any) => (
+                      <div className={styles.item}>
+                          <label>{item}</label>
+                          <span />
+                      </div>
+                  ))
+                : section.items.map((item: any) => (
+                      <div className={styles.item}>
+                          <p>{item}</p>
+                      </div>
+                  ));
+        return (
+            <>
+                <h1>{section.title}</h1>
+                <div className={styles.section}>{content}</div>
+            </>
+        );
+    });
+
+    return (
+        <>
+            <div className={styles.title}>
+                <h1>{`${clientData?.firstName} ${clientData?.lastName}'s Shopping List`}</h1>
+                <h1>
+                    {
+                        // Uses timestamp, if undefined uses today's date
+                        new Date(
+                            visitData
+                                ? visitData?.timestamp?.seconds * 1000
+                                : new Date()
+                        ).toDateString()
+                    }
+                </h1>
+            </div>
+
+            {bodyItems}
+        </>
+    );
+}
