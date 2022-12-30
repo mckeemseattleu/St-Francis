7efdf33a286ca0d4ae6fb5252f7b1648a51e324e
Kevin Chang <kevinjchang98@gmail.com>
2022-12-29 16:29:15 -0800

    update page styling

--- app/add-client/page.tsx
+++ app/add-client/page.tsx
@@ -4,10 +4,8 @@ import ClientInfoForm from '../../components/ClientInfoForm/ClientInfoForm';
 
 export default function AddClient() {
     return (
-        <>
-            <h1>New client</h1>
-
-            <ClientInfoForm showBackButton={false} />
-        </>
+        <div className="container">
+            <ClientInfoForm title="New Client" showBackButton={false} />
+        </div>
     );
 }
