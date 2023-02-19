9c7cba61ba87d276e355211ff1a30849252d32d5
Duc Vo <dvo4@seattleu.edu>
2023-02-19 13:47:57 -0800

    refactor Main component to its own component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
 'use client';
 
-import { useAuth } from '@/hooks/index';
+import { Footer, Main } from '@/components/Layout';
 import Providers from '@/providers/index';
 import Head from 'next/head';
 import NavBar from '../components/NavBar/NavBar';
 import '../styles/globals.css';
-import styles from '../styles/Home.module.css';
 
 export default function RootLayout({ children }: any) {
     return (
@@ -17,26 +16,12 @@ export default function RootLayout({ children }: any) {
             </Head>
 
             <body>
-                <div className={styles.container}>
-                    <Providers>
-                        <NavBar />
-                        <Main>{children}</Main>
-                    </Providers>
-                </div>
-
-                {/* <footer className={styles.footer}>
-                    <p>Footer content</p>
-                </footer> */}
+                <Providers>
+                    <NavBar />
+                    <Main>{children}</Main>
+                    <Footer enabled />
+                </Providers>
             </body>
         </html>
-    );
-}
-
-function Main({ children }: { children: React.ReactNode }) {
-    const { isSignedIn } = useAuth();
-    return (
-        <main className={styles.main}>
-            {isSignedIn ? children : <h1>Please sign in</h1>}
-        </main>
     );
 }
