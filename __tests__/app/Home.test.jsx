8d4a9bb69d86d54192f710193698401734010855
Duc Vo <dvo4@seattleu.edu>
2023-02-25 19:44:47 -0800

    add query client provider for required component testing

--- __tests__/app/Home.test.jsx
+++ __tests__/app/Home.test.jsx
@@ -1,13 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Home from '../../app/page';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 // Fake Login component
 jest.mock('../../components/Login/Login');
 
 describe('Home', () => {
+    const queryClient = new QueryClient();
     it('renders a title', () => {
-        render(<Home />);
+        render(<QueryClientProvider client={queryClient}><Home /></QueryClientProvider>);
 
         const title = screen.getByRole('heading', {
             name: 'St. Francis House',
--- __tests__/app/checkedin/CheckedIn.test.jsx
+++ __tests__/app/checkedin/CheckedIn.test.jsx
@@ -1,10 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import CheckedIn from '../../../app/checkedin/page';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 describe('CheckedIn', () => {
+    const queryClient = new QueryClient();
     it('renders a title', () => {
-        render(<CheckedIn />);
+        render(<QueryClientProvider client={queryClient}><CheckedIn /></QueryClientProvider>);
 
         const title = screen.getByRole('heading', {
             name: 'Checked in clients',
