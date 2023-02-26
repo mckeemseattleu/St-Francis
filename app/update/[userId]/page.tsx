c7b36c76cb4d2a278c0a89821a63b44699e2307f
Duc Vo <dvo4@seattleu.edu>
2023-02-25 19:44:05 -0800

    decoupling form onSave and onSaveAndCheck

--- app/update/[userId]/page.tsx
+++ app/update/[userId]/page.tsx
@@ -1,63 +1,52 @@
 'use client';
 
-import { doc, getDoc } from 'firebase/firestore';
-import { useEffect, useState } from 'react';
-import { firestore } from '../../../firebase/firebase';
-import { useRouter } from 'next/navigation';
 import { ClientInfoForm } from '@/components/Client/index';
-import { ClientDoc } from '../../profile/[userId]/page';
+import Spinner from '@/components/Spinner/Spinner';
+import { DocFilter, getClient, updateClient } from '@/utils/index';
+import { useRouter } from 'next/navigation';
+import { useQuery } from 'react-query';
 
 interface UpdateProps {
     params: { userId: string };
 }
 
 export default function Update({ params }: UpdateProps) {
+    const { isLoading, data } = useQuery(
+        ['selected-clients', params.userId],
+        () => getClient(params.userId)
+    );
     const router = useRouter();
-    const [oldClientData, setOldClientData] = useState<ClientDoc>();
 
-    // Get client data on component load
-    useEffect(() => {
-        getClientData();
-    }, []);
-
-    // Gets the client's document from firestore based on route's userId
-    const getClientData = async () => {
-        const clientDoc = await getDoc(
-            doc(firestore, 'clients', params.userId)
-        );
-
-        // Set local state if their doc exists, otherwise go back to homepage
-        if (clientDoc.exists()) {
-            setOldClientData({
-                firstName: clientDoc.data().firstName,
-                lastName: clientDoc.data().lastName,
-                firstNameLower: clientDoc.data().firstNameLower,
-                lastNameLower: clientDoc.data().lastNameLower,
-                middleInitial: clientDoc.data().middleInitial,
-                birthday: clientDoc.data().birthday,
-                gender: clientDoc.data().gender,
-                race: clientDoc.data().race,
-                postalCode: clientDoc.data().postalCode,
-                numKids: clientDoc.data().numKids,
-                notes: clientDoc.data().notes,
-                isCheckedIn: clientDoc.data().isCheckedIn,
-                isBanned: clientDoc.data().isBanned,
-            });
-        } else {
-            router.push('/');
-        }
+    const onUpdate = async (clientData: DocFilter) => {
+        // save existing client and redirect to profile page
+        await updateClient(clientData);
+        router.push(`/profile/${clientData.id}`);
     };
+
+    const onSaveAndCheck = async (clientData: DocFilter) => {
+        // save and check-out, redirect to profile page
+        if (clientData.isCheckedIn) {
+            clientData.isCheckedIn = false;
+            await onUpdate(clientData);
+            return;
+        }
+
+        // save and redirect to check-in page
+        await updateClient(clientData);
+        router.push(`/checkin/${clientData.id}`);
+    };
+
+    if (isLoading) return <Spinner />;
+    if (!data) return <>Client Not Found</>;
 
     return (
         <div className="container">
-            {oldClientData ? (
-                <ClientInfoForm
-                    id={params.userId}
-                    initialData={oldClientData}
-                    redirect={`/profile/${params.userId}`}
-                    title={'Update Client Form'}
-                />
-            ) : null}
+            <ClientInfoForm
+                initialData={data}
+                title={'Update Client Form'}
+                onSave={onUpdate}
+                onSaveAndCheck={onSaveAndCheck}
+            />
         </div>
     );
 }
