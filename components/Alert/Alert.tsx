4e7b49f1b3d03216caa3cf38dbc5ca13e8caccce
Duc Vo <dvo4@seattleu.edu>
2023-04-23 20:01:18 -0700

    create alert ui component

---
+++ components/Alert/Alert.tsx
@@ -0,0 +1,26 @@
+import styles from './Alert.module.css';
+
+type AlertProps = {
+    children?: React.ReactNode;
+    open?: boolean;
+    onClose?: () => void;
+    type: 'error' | 'warning' | 'info' | 'success' | undefined;
+};
+
+function Alert(props: AlertProps) {
+    const { open, children, onClose, type = 'success' } = props;
+
+    return (
+        (open && (
+            <div
+                className={`${styles.alert} ${styles[type]}`}
+                onClick={onClose}
+            >
+                {children}
+            </div>
+        )) ||
+        null
+    );
+}
+
+export default Alert;
