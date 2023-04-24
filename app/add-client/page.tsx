b4a6b8194483d89428e93108674ae6371e9d7b6d
Duc Vo <dvo4@seattleu.edu>
2023-04-23 20:40:57 -0700

    add user alert notification for new client creation

--- app/add-client/page.tsx
+++ app/add-client/page.tsx
@@ -1,21 +1,37 @@
 'use client';
 import { ClientInfoForm } from '@/components/Client/index';
+import { useAlert } from '@/hooks/index';
 import { createClient, DocFilter } from '@/utils/index';
 import { useRouter } from 'next/navigation';
 
 export default function AddClient() {
     const router = useRouter();
+    const [, setAlert] = useAlert();
 
+    // Transform Client data with birthday converted to UTC
+    const transformData = (clientData: DocFilter) => ({
+        ...clientData,
+        birthday: new Date(clientData.birthday as string),
+    });
+
+    // save new client and redirect to profile page
     const onSave = async (clientData: DocFilter) => {
-        // save new client and redirect to profile page
-        const id = await createClient(clientData);
-        router.push(`/profile/${id}`);
+        const client = await createClient(transformData(clientData));
+        setAlert({
+            message: 'Client Successfully Created.',
+            type: 'success',
+        });
+        router.push(`/profile/${client.id}`);
     };
 
+    // save new client and redirect to check-in page
     const onSaveAndCheck = async (clientData: DocFilter) => {
-        // save new client and redirect to check-in page
-        const id = await createClient(clientData);
-        router.push(`/checkin/${id}`);
+        const client = await createClient(transformData(clientData));
+        setAlert({
+            message: 'Client Created, Check-In Now',
+            type: 'success',
+        });
+        router.push(`/checkin/${client.id}`);
     };
 
     return (
