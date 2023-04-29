9fde526530a313656c5ee79932876600a4efa87d
Duc Vo <dvo4@seattleu.edu>
2023-04-28 23:51:36 -0700

    modify settings provider to use created helper functions

--- providers/SettingsProvider.tsx
+++ providers/SettingsProvider.tsx
@@ -1,57 +1,46 @@
+import { Settings } from '@/models/index';
+import { getSettings, updateSettings } from '@/utils/index';
 import { createContext, useEffect, useState } from 'react';
-import { doc, getDoc, setDoc } from 'firebase/firestore';
-import { firestore } from '../firebase/firebase';
-export interface Settings {
-    daysEarlyThreshold: number;
-    backpackThreshold: number;
-    sleepingBagThreshold: number;
-    earlyOverride: boolean;
-}
 
-export interface SettingsContext {
-    settings: Settings;
-    updateSettings: Function;
-}
+export type SettingsContext = {
+    settings?: Settings;
+    saveSettings: Function;
+};
 
 export const SettingsContext = createContext({} as SettingsContext);
 
-interface SettingsProviderProps {
+type SettingsProviderProps = {
     children: React.ReactNode;
-}
+};
 
+/**
+ * SettingsProvider component is a provider for settings context.
+ * Contains settings data and CRUD API for settings.
+ */
 export default function SettingsProvider(props: SettingsProviderProps) {
     const { children } = props;
 
-    const [settings, setSettings] = useState<Settings>({
-        daysEarlyThreshold: 0,
-        backpackThreshold: 0,
-        sleepingBagThreshold: 0,
-        earlyOverride: false,
-    });
+    const [settings, setSettings] = useState<Settings>();
 
-    const getSettings = async () => {
-        const settingsDoc = await getDoc(doc(firestore, 'settings', 'default'));
-
-        if (settingsDoc.exists()) {
-            setSettings(settingsDoc.data() as Settings);
-        }
+    // Fetch settings from database using helper function
+    const fetchSettings = async () => {
+        const settingsData = await getSettings();
+        setSettings(settingsData as Settings);
     };
 
-    const updateSettings = async (settingsData: Settings) => {
-        await setDoc(doc(firestore, 'settings', 'default'), {
-            ...settings,
-            ...settingsData,
-        });
-        await getSettings();
+    // Update settings in database using helper function
+    const saveSettings = async (settingsData: Settings) => {
+        await updateSettings({ ...settings, ...settingsData });
+        await fetchSettings();
     };
 
     useEffect(() => {
-        getSettings();
+        fetchSettings();
     }, []);
 
     const values = {
         settings: settings,
-        updateSettings: updateSettings,
+        saveSettings: saveSettings,
     };
 
     return (
