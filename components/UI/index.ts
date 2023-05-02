a78656a244a6cfa9a92fecbb32495dd96f0f8e50
Duc Vo <dvo4@seattleu.edu>
2023-05-02 15:08:09 -0700

    export from ui elements from index

---
+++ components/UI/index.ts
@@ -0,0 +1,6 @@
+import Form from './Form/Form';
+import FormTitle from './Form/FormTitle';
+import FormRow from './Form/FormRow';
+import FormItem from './Form/FormItem';
+import Button from './Button/Button';
+export { Form, FormTitle, FormRow, FormItem, Button };
