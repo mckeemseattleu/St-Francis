8dd0229254fedc90609601ed89eed598a1ee2141
Duc Vo <dvo4@seattleu.edu>
2023-05-04 14:22:06 -0700

    export Modal component from index

--- components/UI/index.ts
+++ components/UI/index.ts
@@ -3,4 +3,5 @@ import FormTitle from './Form/FormTitle';
 import FormRow from './Form/FormRow';
 import FormItem from './Form/FormItem';
 import Button from './Button/Button';
-export { Form, FormTitle, FormRow, FormItem, Button };
+import Modal from './Modal/Modal';
+export { Form, FormTitle, FormRow, FormItem, Button, Modal };
