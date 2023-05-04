115ded48e12e32d536faa5db74980f11b3747248
Duc Vo <dvo4@seattleu.edu>
2023-05-04 14:15:01 -0700

    export modules from utils

--- utils/index.ts
+++ utils/index.ts
@@ -1,32 +1,29 @@
-import type { DocFilter } from './fetchData';
-import { fetchData, mutateData } from './fetchData';
-import {
+export {
+    CLIENTS_LIMIT,
+    CLIENTS_PATH,
+    SETTINGS_ID,
+    SETTINGS_PATH,
+    VISITS_LIMIT,
+    VISITS_PATH,
+} from './constants';
+export { fetchData, mutateData } from './fetchData';
+export type { DocFilter } from './fetchData';
+export { formatDate, toUTCDateString } from './formatDate';
+export {
     createClient,
     updateClient,
+    deleteClient,
     createVisit,
     updateVisit,
+    deleteVisit,
     updateSettings,
+    reviseClient,
 } from './mutations';
-import {
+export {
     getClient,
+    getVisit,
     listClients,
     listVisits,
-    getVisit,
     getSettings,
 } from './queries';
-
-export type { DocFilter };
-export {
-    fetchData,
-    mutateData,
-    listClients,
-    getClient,
-    createClient,
-    updateClient,
-    listVisits,
-    getVisit,
-    createVisit,
-    updateVisit,
-    getSettings,
-    updateSettings,
-};
+export { validateClient } from './validate';
