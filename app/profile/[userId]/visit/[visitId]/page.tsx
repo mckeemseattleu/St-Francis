c661e4cfa85ca65da299186bbd1667dd8c52dfd9
Duc Vo <dvo4@seattleu.edu>
2023-05-04 14:30:34 -0700

    refactor visit detail to its own component

--- app/profile/[userId]/visit/[visitId]/page.tsx
+++ app/profile/[userId]/visit/[visitId]/page.tsx
@@ -1,12 +1,18 @@
 'use client';
 
 import Spinner from '@/components/Spinner/Spinner';
-import { Button } from '@/components/UI';
+import { Button, Modal } from '@/components/UI';
+import VisitDetail from '@/components/Visit/VisitDetail';
 import VisitInfoForm from '@/components/Visit/VisitInfoForm';
 import { useAlert } from '@/hooks/index';
 import { Visit as VisitModel } from '@/models/index';
-import { deleteVisit, updateVisit } from '@/utils/mutations';
-import { CLIENTS_PATH, getVisit, VISITS_PATH } from '@/utils/queries';
+import {
+    CLIENTS_PATH,
+    deleteVisit,
+    getVisit,
+    updateVisit,
+    VISITS_PATH,
+} from '@/utils/index';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
@@ -20,6 +26,8 @@ interface VisitProps {
 export default function Visit({ params }: VisitProps) {
     const router = useRouter();
     const [, setAlert] = useAlert();
+    const [show, setShow] = useState(false);
+
     const {
         isLoading,
         data: visitData,
@@ -28,7 +36,7 @@ export default function Visit({ params }: VisitProps) {
         [CLIENTS_PATH, params.userId, VISITS_PATH, params.visitId],
         () => getVisit(params.userId, params.visitId)
     );
-    const [edit, setEdit] = useState(false);
+    const [editVisit, setEditVisit] = useState(false);
 
     const removeVisit = async () => {
         await deleteVisit(params.visitId, params.userId);
@@ -39,111 +47,57 @@ export default function Visit({ params }: VisitProps) {
     if (isLoading) return <Spinner />;
 
     const handleSubmit = async (data: VisitModel) => {
+        //TODO: allow update of visit date (aka createdAt)
         await updateVisit(data, params.userId);
-        setAlert({ message: 'Visit updated', type: 'success' });
+        setAlert({ message: 'Successfully updated Visit', type: 'success' });
         refetch();
-        setEdit(false);
+        setEditVisit(false);
     };
 
+    const visitDate = visitData?.createdAt?.toDate()?.toLocaleString();
+
     return (
-        <>
-            <h1>Visit Details</h1>
+        <div>
             <div>
-                {visitData?.createdAt ? (
-                    <h2>{visitData?.createdAt?.toDate()?.toLocaleString()}</h2>
-                ) : null}
+                <h1>Visit Details</h1>
+                {visitData?.createdAt && <h2>{visitDate}</h2>}
             </div>
-            {(edit && (
+            <hr />
+            <Button onClick={() => router.push(`/profile/${params.userId}`)}>
+                Back to Profile
+            </Button>
+            {editVisit ? (
                 <VisitInfoForm
                     initialVisitData={visitData}
                     onSubmit={handleSubmit}
                 />
-            )) || (
-                <>
-                    <h2>Clothing</h2>
-                    {visitData?.clothingBoy ||
-                    visitData?.clothingWomen ||
-                    visitData?.clothingBoy ||
-                    visitData?.clothingGirl ? (
-                        <>
-                            <p>{visitData?.clothingMen ? 'Men' : null}</p>
-                            <p>{visitData?.clothingWomen ? 'Women' : null}</p>
-                            <p>
-                                {visitData?.clothingBoy ? 'Kids (boy)' : null}
-                            </p>
-                            <p>
-                                {visitData?.clothingGirl ? 'Kids (girl)' : null}
-                            </p>
-                        </>
-                    ) : (
-                        <p>None</p>
-                    )}
-
-                    <h2>Special Requests</h2>
-                    {visitData?.backpack ||
-                    visitData?.sleepingBag ||
-                    visitData?.busTicket ||
-                    visitData?.giftCard ||
-                    visitData?.diaper ||
-                    visitData?.financialAssistance ? (
-                        <>
-                            <p>{visitData?.backpack ? 'Backpack' : null}</p>
-                            <p>
-                                {visitData?.sleepingBag ? 'Sleeping Bag' : null}
-                            </p>
-                            <p>
-                                {visitData?.busTicket
-                                    ? `Bus Tickets: ${visitData?.busTicket}`
-                                    : null}
-                            </p>
-                            <p>
-                                {visitData?.giftCard
-                                    ? `Gift Card: ${visitData?.giftCard}`
-                                    : null}
-                            </p>
-                            <p>
-                                {visitData?.diaper
-                                    ? `Diapers: ${visitData?.diaper}`
-                                    : null}
-                            </p>
-                            <p>
-                                {visitData?.financialAssistance
-                                    ? `Financial Assistance: ${visitData?.financialAssistance}`
-                                    : null}
-                            </p>
-                        </>
-                    ) : (
-                        <p>None</p>
-                    )}
-
-                    <h2>Household items</h2>
-                    <p>
-                        {visitData?.household?.length === 0
-                            ? 'None'
-                            : visitData?.household}
-                    </p>
-
-                    <h2>Notes</h2>
-                    <p>
-                        {visitData?.notes?.length === 0
-                            ? 'None'
-                            : visitData?.notes}
-                    </p>
-                </>
+            ) : (
+                <VisitDetail visitData={visitData as VisitModel} />
             )}
-
             <h2>Options</h2>
             <div className={styles.rowContainer}>
-                <Button onClick={() => setEdit(!edit)}>
-                    {!edit ? 'Edit Visit' : 'Cancel Edit'}
+                <Button onClick={() => setEditVisit(!editVisit)}>
+                    {!editVisit ? 'Edit Visit' : 'Cancel Edit'}
                 </Button>
-                <Button onClick={removeVisit}>Delete Visit</Button>
+                <Button onClick={() => setShow(true)}>Delete Visit</Button>
                 <Link
                     href={`/profile/${params.userId}/visit/${params.visitId}/printout`}
                 >
                     <Button>Printout</Button>
                 </Link>
             </div>
-        </>
+            <Modal show={show} setShow={setShow}>
+                <h3>Are you sure you want to delete this visit?</h3>
+                <hr />
+                <h4>
+                    Visit Date:{' '}
+                    {visitData?.createdAt?.toDate()?.toLocaleString()}
+                </h4>
+                <div className={styles.rowContainer}>
+                    <Button onClick={() => setShow(false)}>Cancel</Button>{' '}
+                    <Button onClick={removeVisit}>Confirm Delete</Button>
+                </div>
+            </Modal>
+        </div>
     );
 }
