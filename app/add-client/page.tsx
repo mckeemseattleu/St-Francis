b29ad00a5cad436f0063d431cec28b713e6b7f67
lmeng25 <lmeng2@seattleu.edu>
2023-12-07 23:55:02 -0800

    check for duplicate when creating new clients

--- app/add-client/page.tsx
+++ app/add-client/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { ClientInfoForm } from '@/components/Client/index';
 import { useAlert } from '@/hooks/index';
-import { createClient, DocFilter } from '@/utils/index';
+import { createClient, DocFilter, fetchData } from '@/utils/index';
 import { useRouter } from 'next/navigation';
 
 export default function AddClient() {
@@ -14,9 +14,40 @@ export default function AddClient() {
         birthday: new Date(clientData.birthday as string),
     });
 
+    // check for duplicate client using (fistName + lastName + birthday + gender)
+    const checkForDuplicateClient = async (clientData: DocFilter) => {
+        try {
+            const filter = {
+                firstNameLower: clientData.firstNameLower,
+                lastNameLower: clientData.lastNameLower,
+                birthday: clientData.birthday,
+                gender: clientData.gender,
+            };
+            const clients = await fetchData(filter);
+            return Array.isArray(clients) && clients.length > 0;
+        } catch (err: any) {
+            setAlert({
+                message: err.message,
+                type: 'error',
+            });
+        }
+    };
+
     // save new client and redirect to profile page
     const onSave = async (clientData: DocFilter) => {
         try {
+            // alert if duplicate client already exists
+            const isDuplicate = await checkForDuplicateClient(
+                transformData(clientData)
+            );
+            if (isDuplicate) {
+                setAlert({
+                    message: 'Duplicate client detected. Client not created.',
+                    type: 'error',
+                });
+                return;
+            }
+
             const client = await createClient(transformData(clientData));
             setAlert({
                 message: 'Client Successfully Created.',
@@ -34,6 +65,18 @@ export default function AddClient() {
     // save new client and redirect to check-in page
     const onSaveAndCheck = async (clientData: DocFilter) => {
         try {
+            // alert if duplicate client already exists
+            const isDuplicate = await checkForDuplicateClient(
+                transformData(clientData)
+            );
+            if (isDuplicate) {
+                setAlert({
+                    message: 'Duplicate client detected. Client not created.',
+                    type: 'error',
+                });
+                return;
+            }
+
             const client = await createClient(transformData(clientData));
             setAlert({
                 message: 'Client Created, Check-In Now',
